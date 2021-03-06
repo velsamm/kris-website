9254019ab3d462b11556f9694676e84a38bfe7b6
Aleksandr Diskant <velsam@bitzlato.com>
2021-03-07 01:59:55 +0300

    add robots.txt

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
@@ -17,6 +18,14 @@ module.exports = {
         new MiniCssExtractPlugin({ filename: 'index.css' }),
         new HtmlWebpackPlugin({
             template: 'public/index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'public/robots.txt',
+                    to: 'robots.txt',
+                },
+            ],
         }),
     ],
 
@@ -49,6 +58,10 @@ module.exports = {
                 generator: {
                     filename: 'static/[hash][ext][query]',
                 },
+            },
+            {
+                test: /.(txt)$/,
+                type: 'asset/resource',
             },
         ],
     },
