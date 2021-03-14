edde89b1085c2507d03c91d66bcaa5440f8aea6e
Aleksandr Diskant <velsam@bitzlato.com>
2021-03-14 22:54:28 +0300

    add favicon. edit title and meta description

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,34 @@ module.exports = {
                     from: 'public/sitemap.xml',
                     to: 'sitemap.xml',
                 },
+                {
+                    from: 'public/android-chrome-192x192.png',
+                    to: 'android-chrome-192x192.png',
+                },
+                {
+                    from: 'public/android-chrome-512x512.png',
+                    to: 'android-chrome-512x512.png',
+                },
+                {
+                    from: 'public/apple-touch-icon.png',
+                    to: 'apple-touch-icon.png',
+                },
+                {
+                    from: 'public/favicon.ico',
+                    to: 'favicon.ico',
+                },
+                {
+                    from: 'public/favicon-16x16.png',
+                    to: 'favicon-16x16.png',
+                },
+                {
+                    from: 'public/favicon-32x32.png',
+                    to: 'favicon-32x32.png',
+                },
+                {
+                    from: 'public/site.webmanifest',
+                    to: 'site.webmanifest',
+                },
             ],
         }),
     ],
